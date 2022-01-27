cdcd6627a8836bda63eb77b9a124c91aa3033577
fabrizioCast <waynesleek3@gmail.com>
2022-01-26 22:17:28 -0500

    fix env

--- src/utils/getCurrentLocation.js
+++ src/utils/getCurrentLocation.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default async function getCurrentLocation() {
   try {
     const res = await axios.get(
-      `https://api.ipdata.co/?api-key=${process.env.KEY_API}`
+      `https://api.ipdata.co/?api-key=${process.env.REACT_APP_KEY_API}`
     );
     return res.data.city;
   } catch (e) {
