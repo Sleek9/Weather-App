5e0e009bb053230987b4de0da3adc7b0355235ce
fabrizioCast <waynesleek3@gmail.com>
2022-01-26 21:45:41 -0500

    fix url ip-api.com

--- src/utils/getCurrentLocation.js
+++ src/utils/getCurrentLocation.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default async function getCurrentLocation() {
   try {
-    const res = await axios.get("http://ip-api.com/json");
+    const res = await axios.get("https://ip-api.com/json");
     return res.data.city;
   } catch (e) {
     return null;
